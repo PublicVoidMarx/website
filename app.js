bad7f6cf175e998adbe5ee1b3a348095bc608a31
PublicVoidMarx <luchigroso9@hotmail.com>
2021-09-12 12:41:28 -0300

    middleware and front-end conection

--- app.js
+++ app.js
@@ -1,40 +1,30 @@
-const app = require('express')()
+const express = require('express')
 const path = require('path')
-const {people} = require('./data')
+const bodyParser = require('body-parser')
+const people = require('./routes/people')
 
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('./public')) //home page
+
+app.use(express.json())
+
+app.use('/api/people',people)
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./index.html'))
+    res.status(200).sendFile(path.join(
+        __dirname,'index.html'))
 })
 
-app.get('/data/:peopleID',(req,res)=>{
-    const {peopleID} = req.params
-    const person = people.find((people)=>people.id === Number(peopleID))
-    if (!person) {
-        return res.status(404).send('Person does not exist')
-    }
-    //data = [person.id,person.email,person.name]
-    return res.json(person)
+app.get('/about',(req,res)=>{
+    res.status(200).sendFile(path.join(
+        __dirname,'about.html'))
 })
 
-app.get('/api/v1/query',(req,res)=>{
-    console.log(req.query)
-    const {search,limit} = req.query
-    let sortedPeople = [...people]
-
-    if (search) {
-        sortedPeople = sortedPeople.filter((people)=>{
-            return people.id > search
-        })
-    }
-    if(limit){
-        sortedPeople = sortedPeople.slice(0,Number(limit))
-    }
-    if(sortedPeople.length<1) {
-        //res.status(200).send('No products match query')
-        return res.status(200).json({succes:true,data:[]})
-    }
-    res.status(200).json(sortedPeople)
+app.post('/login',(req,res)=>{
+    console.log(req.body)
+    res.end('yes')
 })
 
 app.listen(3000,(req,res)=>{
